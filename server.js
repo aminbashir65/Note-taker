6e11094e6ffeaca984f28468d594927be123df42
amin bashir <aminbashir65@gmail.com>
2021-03-03 11:59:38 -0600

    wrote the routes for db

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var PORT = process.env.PORT || 3001;
 var express = require("express");
 var app = express();
 var path = require("path");
+var fs = require("fs");
 
 // express middleware
 app.use(express.urlencoded({
@@ -17,7 +18,13 @@ app.get("/", function(req, res) {
   app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
   });
-
+app.get("/api/notes", function(req, res) {
+    fs.readFile("db/db.json", "utf-8", function(err, data){
+        if (err) throw err
+        var notes = JSON.parse(data)
+       res.json(notes);
+    })
+  });
 app.listen(PORT, function() {
     console.log("app is listening on port" + PORT)
 })
