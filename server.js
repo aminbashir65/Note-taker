7363a6e9a22150c8884d6b81bf458d80bcf3d467
amin bashir <aminbashir65@gmail.com>
2021-03-04 12:49:49 -0600

    setup the routes

--- server.js
+++ server.js
@@ -1,63 +1,18 @@
 var express = require("express");
+
+// Sets up the Express App
+// =============================================================
 var app = express();
-var path = require("path");
-var fs = require("fs");
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 3001; 
 
-
-// Express middleware
-app.use(express.urlencoded({
-    extended: true
-}))
+// Sets up the Express app to handle data parsing
 app.use(express.json());
-app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-// Routes
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-  });
-  app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-  });
-app.get("/api/notes", function(req, res) {
-    fs.readFile("db/db.json", "utf-8", function(err, data){
-        if (err) throw err
-        var notes = JSON.parse(data)
-       res.json(notes);
-    })
-  });
-app.listen(PORT, function() {
-    console.log("app is listening on port" + PORT)
-})
-// POST method route 
-app.post("/api/notes", function(req, res) {
+require("./routes/htmlroutes")(app);
+require("./routes/apiroutes")(app);
 
-    let newNote = req.body;
-    let uniqueId = (data.length).toString();
-    console.log(uniqueId);
-    newNote.id = uniqueId;
-    data.push(newNote);
-
-    // This function will write the note into db.json
-    fs.writeFileSync("./db/db.json", JSON.stringify(data), function(err) {
-        if (err) throw (err);        
-    });
-    res.json(data);    
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
 });
-
-// This function deletes notes from the list 
-app.delete("/api/notes/:id", function(req, res) {
-
-    let noteId = req.params.id;
-    let newId = 0;
-    console.log(`Deleting note with id ${noteId}`);
-    data = data.filter(currentNote => {
-       return currentNote.id != noteId;
-    });
-    for (currentNote of data) {
-        currentNote.id = newId.toString();
-        newId++;
-    }
-    fs.writeFileSync("./db/db.json", JSON.stringify(data));
-    res.json(data);
-}); 
